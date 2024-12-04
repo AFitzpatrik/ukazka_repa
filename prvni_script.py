7a17717adcd1004151a64a52b38c2db37e0b822c
franc <francupatrik@seznam.cz>
2024-12-04 18:35:04 +0100

    Zadání úkolu, push

--- prvni_script.py
+++ prvni_script.py
@@ -6,3 +6,13 @@ print("třetí úprava")
 
 print("Commit 4")
 
+
+
+
+
+
+#Úkol GIT -
+#1 - Na githubu/gitlabu si vytvořte repozitář
+#2 - "Naklonujte si repozitář" - přes pycharm File- "Project from version control"
+#3 - Vytvořte nový soubor, zacommitujte ho a klikněte na commit and push
+#4 - do tohoto vlákna mi pošlete URL na váš repozitář
